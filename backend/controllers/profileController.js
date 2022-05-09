807e645bc9a4fc7349fd714dbe88b3765437c38d
OmarHoussami <omar.houssami20@gmail.com>
2022-05-09 13:00:54 +0300

    added profile controller

---
+++ backend/controllers/profileController.js
@@ -0,0 +1,35 @@
+const { User } = require("../models/user");
+const { validateNameChangeInput } = require('../helpers/inputValidation');
+
+
+
+changeName = (req, res) => {
+  const { FirstName, LastName } = req.body;
+  const { errors, isValid } = validateNameChangeInput(req.body);
+  if (!isValid) {
+    res.status(400).json({ ...errors, success: false });
+  } else {
+    User.findById(req.user.id)
+      .then((dbUser) => {
+        if (!dbUser) {
+          res.status(400).json({ message: 'User not found', success: false });
+        }
+        else {
+          dbUser.FirstName = FirstName;
+          dbUser.LastName = LastName;
+          dbUser.save()
+            .then(() => {
+              res.status(202).json({ message: 'Name updated successfully', success: true });
+            })
+            .catch((err) => {
+              res.status(500).json({ message: `Error occurred while saving to database : ${err}` });
+            });
+        }
+      })
+      .catch((err) => {
+        res.status(500).json({ message: `Error occurred while searching for user : ${err}` });
+      })
+  }
+};
+
+module.exports = { changeName };
