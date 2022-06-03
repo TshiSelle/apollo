b5ff667daa9c5393ad219a0baa1a8e996ebb0b6a
OmarHoussami <omar.houssami20@gmail.com>
2022-06-03 10:17:08 +0300

    updated user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,24 +1,24 @@
 const isEmpty = require('is-empty');
 const { User } = require('../models/user');
 
-
 const getUser = (req, res) => {
     const { id } = req.user;
     if (isEmpty(id)) {
         res.status(400).json({ message: 'No ID found', success: false });
     }
-    User.findOne({ _id: id }, {
-        Password: 0,
-        emailVerificationToken: 0,
-        passResetToken: 0,
-        passResetTokenExpirationDate: 0,
-        __v: 0,
-        _id: 0,
-        Verified: 0,
-        Writer: 0,
-        createdAt: 0,
-        updatedAt: 0
-    })
+    User.findOne(
+        { _id: id },
+        {
+            password: 0,
+            emailVerificationToken: 0,
+            passResetToken: 0,
+            passResetTokenExpirationDate: 0,
+            writer: false,
+            __v: 0,
+            _id: 0,
+            updatedAt: 0,
+        }
+    )
         .then((dbUser) => {
             if (dbUser) {
                 res.status(200).json({ message: 'User found', success: true, dbUser });
@@ -27,8 +27,11 @@ const getUser = (req, res) => {
             }
         })
         .catch((err) => {
-            res.status(400).json({ message: 'Error occurred while searching for user', success: false });
-        })
-}
+            res.status(400).json({
+                message: 'Error occurred while searching for user',
+                success: false,
+            });
+        });
+};
 
 module.exports = { getUser };
