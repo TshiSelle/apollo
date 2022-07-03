718fd2ea194c43aad54baf376d99246038a5dd95
OmarHoussami <omar.houssami20@gmail.com>
2022-07-03 18:26:01 +0300

    modified intro content

--- src/components/HomePage/mainsubject.jsx
+++ src/components/HomePage/mainsubject.jsx
@@ -1,21 +1,20 @@
-import React from "react";
-import "./mainsubject.css";
+import React from 'react';
+import './mainsubject.css';
 
 function MainSubject() {
   return (
     <>
-      <div className="mainsubject">
-        <div className="mainsubject__container">
-        <h1>Welcome</h1>
-        <hr className="mainsubject__hr" />
-        <p>
-          This is an open db project bla bla asdj kjas ndal This is an open db
-          project bla bla asdj kjas ndal This is an open db project bla bla asdj kjas ndal This is an open db
-          project bla bla asdj kjas ndal
-          This is an open db project bla bla asdj kjas ndal This is an open db
-          project bla bla asdj kjas ndal This is an open db project bla bla asdj kjas ndal This is an open db
-          project bla bla asdj kjas ndal.
-        </p>
+      <div className='mainsubject'>
+        <div className='mainsubject__container'>
+          <h1>Welcome</h1>
+          <hr className='mainsubject__hr' />
+          <p>
+            Welcome to Project Apollo. The portal made for developers by
+            developers. Apollo is an open-database project that aims to help
+            every individual who strive to improve their technical skills in
+            JavaScript by progressing through community-made documentations and
+            practice.
+          </p>
         </div>
       </div>
     </>
